bf0d08f148cffdb0239de59d29e6da45cde42257
prescottbreeden <prescott.breeden@gmail.com>
2018-07-04 02:29:50 -0700

    raising custom error messages

---
+++ errors/raise.py
@@ -0,0 +1,19 @@
+def annoying_number_guesser(num):
+    if num is not 8:
+        raise ValueError("That's not my number")
+    else:
+        print("You're amazing!")
+
+
+# annoying_number_guesser(3)
+annoying_number_guesser(8)
+
+'''
+Common Errors:
+-------------
+NameError
+ValueError
+TypeError
+AttributeError
+
+'''
