cb6794c1e88800fbc10d51dd6cb15a1785d9afa6
prescottbreeden <prescott.breeden@gmail.com>
2018-07-05 13:39:38 -0700

    class methods and repr

---
+++ oop/pet.py
@@ -0,0 +1,19 @@
+class Pet(object):
+
+    allowed = ['cat', 'dog', 'fish', 'rat']
+
+    def __init__(self, name, species):
+        if species not in Pet.allowed:
+            raise ValueError(f"You cannot have a {species} pet")
+        self.name = name
+        self.species = species
+
+    def set_species(self, species):
+        if species not in Pet.allowed:
+            raise ValueError(f"You cannot have a {species} pet")
+        self.species = species
+
+
+cat = Pet("Blue", "cat")
+dog = Pet("Rex", "dog")
+# tiger = Pet("Jeff", "tiger")
